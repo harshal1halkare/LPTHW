6bd6fb140e3055cff1ea3bc39bfaf3457d5a487e
harshal1halkare <harshal.halkare@izeltech.com>
2016-07-01 18:07:33 +0530

    this is fourth exercise of python

---
+++ ex5.py
@@ -0,0 +1,15 @@
+my_name = "harshal halkare"
+my_age = 24 # its a lie
+my_ht = 6 #its a lie
+my_wt = 70 # its not a lie
+my_eyes = "blue"
+my_teeth = "white"
+my_hair = "black"
+
+print "lets talk about %s." % my_name
+print "he is %d inches tall" % my_ht
+print "he is %d pound heavy" % my_wt
+print "actually dats nt heavy"
+print "he's got %s eyes and %s hair" %(my_eyes,my_hair)
+print "he's teeth are usually %s, depending on coffee "% my_teeth 
+print "if i add %d,%d,and %d I get %d"%(my_age,my_ht,my_wt,my_age + my_ht + my_wt)
