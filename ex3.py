eff79d84287d5437fb241a33580f6b6e113f6228
harshal1halkare <harshal.halkare@izeltech.com>
2016-07-01 18:03:29 +0530

    this is third exercise of python

---
+++ ex3.py
@@ -0,0 +1,14 @@
+print "i will now count my chikens : "
+print "hens",25*30/6
+print "roosters",100-25 * 3 % 4
+print "now i will count the eggs"
+print 3 + 2 + 2 + 1 -5 + 4 % 2 -1 / 4 + 6
+print "it is true that 3 + 2 < 5 - 7?"
+print 3 + 2 < 5 -7
+print "what is 3 + 2?", 3 + 2
+print "what us 5 -7 ?",5-7
+print "oh that is why it's false."
+print "how about some more"
+print "it is greater ?",5> -2
+print "it is greater or equal?",5>= -2
+print"it is less or equal?",5<= -2
---
+++ ex3_1.py
@@ -0,0 +1,4 @@
+print "let us do something with floating points"
+print "this is floating",22.1<=22
+print 22 + 22.222 + 333 / 3 ,"this is the result"
+print 333333.33333
