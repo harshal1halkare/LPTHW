ec355e4a7361cad78bb9c689e31999c13e179dbf
harshal1halkare <harshal.halkare@izeltech.com>
2016-07-04 19:03:53 +0530

    this is exercise ten

---
+++ ex11.py
@@ -0,0 +1,8 @@
+print "how old are you",
+age = int(raw_input())
+print "how tall are you",
+ht = raw_input()
+print "how much do you weigh",
+wt = raw_input()
+
+print "so you are %r old, %r tall, %r heavy." %(age,ht,wt)
---
+++ ex12.py
@@ -0,0 +1,5 @@
+age = raw_input("how old are you ? ")
+ht =  raw_input("how tall are you ?")
+wt = raw_input("how much do you weigh?")
+
+print "so your age is %r, your ht is %r, your wt is %r" %(age,ht,wt)
