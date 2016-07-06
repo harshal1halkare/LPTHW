9a873c98608707f604696395cb46a110c02c1136
harshal1halkare <harshal.halkare@izeltech.com>
2016-07-06 18:51:43 +0530

    this is exercise 13 for argument in python

---
+++ ex13_1.py
@@ -0,0 +1,4 @@
+from sys import argv
+script,first,second,third,fourth,fifth = argv
+sixth = raw_input("please enter your sixth input here")
+print ("ur 1st argv is %r,ur second argv is %r,ur third argv is %r,ur fourth argv is %r,ur fifth argv is %r,ur sixth argv is %r" %(first,second,third,fourth,fifth,sixth))
