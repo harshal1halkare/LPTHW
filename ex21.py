47553685d1c32999bf07f9bac4aab8c5d97f3b38
harshal1halkare <harshal.halkare@izeltech.com>
2016-07-08 16:43:37 +0530

    Functions can return something

---
+++ ex21.py
@@ -0,0 +1,27 @@
+def add(a,b):
+	print "addin %d and %d" %(a,b)
+	return a+b
+	#print "\n"
+
+def sub(a,b):
+	print "subtracting %d and %d" %(a,b)
+	return a - b
+    #print "\n"
+
+def mul(a,b):
+	print("multiplying %d and %d") %(a,b)
+	return a * b
+	#print "\n"
+
+def div(a,b):
+	print("dividing %d and %d") %(a,b)
+	return a / b
+	#print "\n"
+
+age = add(20,30)
+ht = sub (55,20)
+wt = mul (2,55)
+iq = div(20,5)
+
+
+print "so age is %d height is %d  weight is %d  iq is %d " %(age,ht,wt,iq)
