a0de751af5c5554ffb0966b889d3adb5cdd0374a
harshal1halkare <harshal.halkare@izeltech.com>
2016-07-08 16:40:42 +0530

    Functiohns and variable

---
+++ ex19.py
@@ -0,0 +1,22 @@
+def cheese_and_crackers(count_cheese,count_crackers):
+		print "no of cheese available are : %r" % count_cheese
+		print "no of cracker boxes are :  %r "  % count_crackers
+		print "enough stuff for party....cheers"
+
+
+
+print "here is a way to print function directly"
+cheese_and_crackers(20,30)
+
+print"or another way to print is give values separatly"
+amount_of_cheese = 10
+amount_of_crackers = 150
+
+cheese_and_crackers(amount_of_cheese,amount_of_crackers)
+
+print "we can perform maths inside function..its fun"
+cheese_and_crackers(3000+200000,4000+50000)
+
+print "or we can combine math with variable"
+cheese_and_crackers(amount_of_cheese +100000,amount_of_crackers +300000000)
+
