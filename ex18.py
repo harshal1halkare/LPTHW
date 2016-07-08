477dbf6ab2fe99881056446cc084973d63d3cacb
harshal1halkare <harshal.halkare@izeltech.com>
2016-07-08 16:38:12 +0530

    Names,Variable,Function exercise

---
+++ ex18.py
@@ -0,0 +1,42 @@
+def print_number(*args):
+		arg1,arg2 = args
+		print "arg1 value is %r and arg2 value is %r" %(arg1,arg2)
+
+
+def print_number_new_way(arg1,arg2):
+		print "arg1 value is %r and arg2 value is %r" %(arg1,arg2)
+
+def print_number_one_arg(arg1):
+		print "so the only one arg is %r" %arg1
+
+def print_number_no_arg():
+		print "it has no arg so nothing to print "
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print_number_new_way("threeeee","four")
+print_number("first","second")
+print_number_one_arg("only number")
+print_number_no_arg()
