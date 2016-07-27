b3c578aaa097902f082fc89643602f9f16d8b5f0
harshal1halkare <harshal.halkare@izeltech.com>
2016-07-27 16:39:53 +0530

    this is ex 33

---
+++ ex33.py
@@ -0,0 +1,13 @@
+i = 0
+numbers = []
+
+while i < 6:
+	print "at top i is %d" % i
+	numbers.append(i)
+	i = i + 1
+	print "numbers now ", numbers
+	print "at bottom i is %d" % i
+print "the numbers: "
+
+for num in numbers:
+	print num
